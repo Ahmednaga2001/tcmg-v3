234fd09ac52e0026e1bba44334a83ca15d102fae
Ahmednaga2001 <ahmednaga02001@gmail.com>
2024-10-31 16:38:02 +0300

    modify intern page

--- components/hiring/client/hiringWhyChooseUse/WhyChooseAccordion.jsx
+++ components/hiring/client/hiringWhyChooseUse/WhyChooseAccordion.jsx
@@ -12,35 +12,44 @@ const WhyChooseAccordion = ({ title, path, desc }) => {
 
   return (
     <div>
-      <div className={styles.accordionItem} >
-        <div className={`${styles.accordionItemHeader} ${isOpenOne ? styles.open : ""}`} onClick={handleIsOpenOne}>
-            <Image
-              src="/assets/icons/hiring/arrow-right.png"
-              width={33}
-              height={25}
-              alt={title}
-              className={styles.accordionItemIcon}
-            />
-            <span>{title}</span>
-            <Image
-              src="/assets/icons/hiring/arrow-left-icon.png"
-              width={33}
-              height={25}
-              alt={title}
-              className={styles.accordionItemIcon}
-            />
-          </div>
-         
-        <div
-          className={`${styles.accordionContent} ${
-            isOpenOne ? styles.open : ""
-          }`}
-        >
-          <p>{desc}</p>
-      
+    <div className={styles.accordionItem}>
+      <div className={styles.accordionItemHeader} onClick={handleIsOpenOne}>
+        <div className={styles.headerContent}>
+          {/* <Image
+            src={path}
+            width={60}
+            height={60}
+            alt={title}
+            className={styles.accordionItemIcon}
+          /> */}
+          <span>{title}</span>
         </div>
+        <Image
+          src="/assets/icons/strategicrelations/arrow-down.png"
+          alt="Drop Icon"
+          width={11}
+          height={6}
+          className={`${styles.accordionArrowIcon} ${isOpenOne ? "" : ""}`}
+        />
+      </div>
+      <div
+        className={`${styles.accordionContent} ${
+          isOpenOne ? styles.open : ""
+        }`}
+      >
+        <p>{desc}</p>
+        <Image
+          src="/assets/icons/strategicrelations/ep_arrow-down-bold.png"
+
+          width={11}
+          height={6}
+          onClick={handleIsOpenOne}
+          alt="Up Icon"
+          className={styles.bottomIcon}
+        />
       </div>
     </div>
+  </div>
   );
 };
 
