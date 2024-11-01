d689a9565702e94ee3bda6b1fc73cda9fee387a7
Ahmednaga2001 <ahmednaga02001@gmail.com>
2024-11-01 06:21:27 +0200

    finsih some modification

--- components/ui/CheckBox.jsx
+++ components/ui/CheckBox.jsx
@@ -1,51 +1,25 @@
 
-// import styles from "./page.module.css";
-
-// const CheckBox = ({ label, checked, onChange, onBlur, name, error }) => {
-//   return (
-//     <div className={styles.checkWrapper}>
-//       <div className={styles.checkBox}>
-//         <input
-//           type="checkbox"
-//           id={name}
-//           checked={checked} // Control the checkbox using the checked prop
-//           onChange={onChange} // Use Formik's onChange
-//           onBlur={onBlur} // Use Formik's onBlur
-//         />
-//         <label htmlFor={name}>
-//           {label}
-//         </label>
-//       </div>
-//       {error && <div className={styles.error}>{error}</div>} {/* Render error message */}
-//     </div>
-//   );
-// };
-
-// export default CheckBox;
-
 import styles from "./page.module.css";
 
-const CheckBox = ({ label, checked, onChange, onBlur, name, error, onLabelClick }) => {
+const CheckBox = ({ label, checked, onChange, onBlur, name, error }) => {
   return (
     <div className={styles.checkWrapper}>
       <div className={styles.checkBox}>
         <input
           type="checkbox"
           id={name}
-          checked={checked}
-          onChange={onChange}
-          onBlur={onBlur}
+          checked={checked} // Control the checkbox using the checked prop
+          onChange={onChange} // Use Formik's onChange
+          onBlur={onBlur} // Use Formik's onBlur
         />
-        <span
-          className={styles.clickableLabel}
-          onClick={onLabelClick} // Make the text clickable
-        >
+        <label htmlFor={name}>
           {label}
-        </span>
+        </label>
       </div>
-      {error && <div className={styles.error}>{error}</div>}
+      {error && <div className={styles.error}>{error}</div>} {/* Render error message */}
     </div>
   );
 };
 
 export default CheckBox;
+
