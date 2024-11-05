6c4ef99b14f7976e35f2a2127c64c6aea2ef1f97
Ahmednaga2001 <ahmednaga02001@gmail.com>
2024-11-05 05:15:39 +0200

    finsih some modification

--- components/establish/client/establishLegalService/LegalServiceCard.jsx
+++ components/establish/client/establishLegalService/LegalServiceCard.jsx
@@ -11,27 +11,45 @@ const LegalServiceCard = ({ title, path, desc,alt }) => {
 
   return (
     <div>
-      <div className={styles.accordionItem}>
-        <div className={styles.accordionItemHeader} onClick={handleIsOpenOne}>
+    <div className={styles.accordionItem}>
+      <div className={styles.accordionItemHeader} onClick={handleIsOpenOne}>
+        <div className={styles.headerContent}>
           <Image
             src={path}
-            width={70}
-            height={70}
-            alt={alt}
-            quality={100}
+            width={60}
+            height={60}
+            alt={title}
             className={styles.accordionItemIcon}
+            quality={100}
           />
           <span>{title}</span>
         </div>
-        <div
-          className={`${styles.accordionContent} ${
-            isOpenOne ? styles.open : ""
-          }`}
-        >
-          <p>{desc}</p>
-        </div>
+        <Image
+          src="/assets/icons/strategicrelations/arrow-down.png"
+          alt="Drop Icon"
+          width={11}
+          height={6}
+          className={`${styles.accordionArrowIcon} ${isOpenOne ? "" : ""}`}
+        />
+      </div>
+      <div
+        className={`${styles.accordionContent} ${
+          isOpenOne ? styles.open : ""
+        }`}
+      >
+        <p>{desc}</p>
+        <Image
+          src="/assets/icons/strategicrelations/ep_arrow-down-bold.png"
+
+          width={11}
+          height={6}
+          onClick={handleIsOpenOne}
+          alt="Up Icon"
+          className={styles.bottomIcon}
+        />
       </div>
     </div>
+  </div>
   );
 };
 
