c295daa2df2c54dbc9a585e3ea8a1564894cabbd
Ahmednaga2001 <ahmednaga02001@gmail.com>
2024-11-16 00:18:05 +0200

    finsih some mod

--- components/establish/establishAchievement/VisonCard.jsx
+++ components/establish/establishAchievement/VisonCard.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
+import Link from "next/link";
 const VisonCard = ({ visionContentstyle, title, desc }) => {
   return (
     <div className={styles[visionContentstyle]}>
       <h3>{title}</h3>
       <p>{desc}</p>
       <div className={styles.link}>
-        <span className={` link`}>
+        <Link href="legal-advice#reserve" className='link'>
           أسس الان
           <Image
             src="assets/icons/arrow-left.svg"
@@ -15,7 +16,7 @@ const VisonCard = ({ visionContentstyle, title, desc }) => {
             width={20}
             height={20}
           />
-        </span>
+        </Link>
       </div>
     </div>
   );
