d57619f2b3e098c3397c04d0889304df66e246e6
Ahmednaga2001 <ahmednaga02001@gmail.com>
2024-11-19 06:52:07 +0200

    finsih some mod22

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
-import Link from 'next/link'
+"use client";
+import { useRouter } from 'next/navigation'
  
 export default function NotFound() {
+  const router = useRouter()
+  const handle = () => {
+    router.push('/')
+  }
   return (
-  <section className='notFound'>
+  <section className='notFound' onClick={handle}>
    
   </section>
   )
